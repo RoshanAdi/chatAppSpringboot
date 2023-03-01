bac1dc19f28598be37a4fae4ced08c2e27bae07c
adikari <rohshanmahesh1@gmail.com>
2023-03-01 20:48:58 +0530

    private msg configured

--- src/main/java/com/ichat/chatappbackend/handlers/ChatWebSocketHandler.java
+++ src/main/java/com/ichat/chatappbackend/handlers/ChatWebSocketHandler.java
@@ -13,8 +13,8 @@ import java.util.*;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
 
-    private final Map<String, WebSocketSession> idToActiveSession = new HashMap<>();
-    private final Map<String, String> idAndNick = new HashMap<>();
+    private final Map<String, WebSocketSession> idToActiveSession = new HashMap<>();   // stores active sessions and ids
+    private final Map<String, String> idAndNick = new HashMap<>();  //stores nick name and the id for updating list of users
 
 
     public ChatWebSocketHandler() {
@@ -28,41 +28,37 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         JSONObject obj = new JSONObject(message.getPayload());
+        System.out.println("showing obj = "+obj);
 
-        if(Objects.equals(obj.get("senderId").toString(), "l4231rfd2384if9")){
-          session.sendMessage(new TextMessage(message.getPayload().replace("l4231rfd2384if9",session.getId())));
+        if(Objects.equals(obj.get("senderId").toString(), "l4231rfd2384if9")){ // check for the first msg receive when joining someone(the nick name)
+          session.sendMessage(new TextMessage(message.getPayload().replace("l4231rfd2384if9",session.getId()))); //user "joined" msg to the new user
 
             idAndNick.put(session.getId(),obj.get("user").toString());
             JSONObject obj2 = new JSONObject(idAndNick);
-            for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) {
-                otherSession.getValue().sendMessage(new TextMessage(obj2.toString()));
-                if (otherSession.getKey().equals(session.getId())) continue;
+            for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) { //sending public msgs to all
+                otherSession.getValue().sendMessage(new TextMessage(obj2.toString())); //updating user list
+                if (otherSession.getKey().equals(session.getId())) continue;  //loop skips to avoid sending "joined" msg again to the new user
                 otherSession.getValue().sendMessage(new TextMessage(message.getPayload()));
+            }}
+        else if (!(Objects.equals(obj.get("recieverId").toString(), ""))) {  //catching private msgs
+            System.out.println("printing obj = "+obj.toString());
+            session.sendMessage(new TextMessage(message.getPayload()));
+idToActiveSession.get(obj.get("recieverId")).sendMessage(new TextMessage(message.getPayload()));
+        }
 
-            }
+        else {  //public chat msgs
+        for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) {
+            otherSession.getValue().sendMessage(new TextMessage(message.getPayload()));
+        }} System.out.println(message.getPayload());
 
         }
-        else {
-        for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) {
-
-            otherSession.getValue().sendMessage(new TextMessage(message.getPayload()));
-
-        }}
-        System.out.println("printing message = "+message);
-        System.out.println(message.getPayload());
-
-
-                    System.out.println("printing nickname array = "+idAndNick);
-
-    }
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         idToActiveSession.remove(session.getId());
-        System.out.println("session = "+session+" "+status);
         super.afterConnectionClosed(session, status);
         idAndNick.remove(session.getId());
 
-        for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) {
+        for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) { //update userlist upon leaving a user
             JSONObject obj2 = new JSONObject(idAndNick);
             otherSession.getValue().sendMessage(new TextMessage(obj2.toString()));;
 
