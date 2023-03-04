3514ae1fc8d66136da5413c5997003888a612b79
adikari <rohshanmahesh1@gmail.com>
2023-03-04 17:08:42 +0530

    User left message sending added

--- src/main/java/com/ichat/chatappbackend/handlers/ChatWebSocketHandler.java
+++ src/main/java/com/ichat/chatappbackend/handlers/ChatWebSocketHandler.java
@@ -2,6 +2,8 @@ package com.ichat.chatappbackend.handlers;
 
 
 import org.json.JSONObject;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -14,6 +16,7 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
     private final Map<String, WebSocketSession> idToActiveSession = new HashMap<>();   // stores active sessions and ids
     private final Map<String, String> idAndNick = new HashMap<>();  //stores nick name and the id for updating list of users
+    private TextMessage message1;
 
 
     public ChatWebSocketHandler() {
@@ -30,7 +33,8 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
 
         if(Objects.equals(obj.get("senderId").toString(), "l4231rfd2384if9")){ // check for the first msg receive when joining someone(the nick name)
-          session.sendMessage(new TextMessage(message.getPayload().replace("l4231rfd2384if9",session.getId()))); //user "joined" msg to the new user
+            this.message1 = message;
+            session.sendMessage(new TextMessage(message.getPayload().replace("l4231rfd2384if9",session.getId()))); //user "joined" msg to the new user
 
             idAndNick.put(session.getId(),obj.get("user").toString());
             JSONObject obj2 = new JSONObject(idAndNick);
@@ -53,13 +57,15 @@ idToActiveSession.get(obj.get("recieverId")).sendMessage(new TextMessage(message
         }
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        var name = idAndNick.get(session.getId());
         idToActiveSession.remove(session.getId());
         super.afterConnectionClosed(session, status);
         idAndNick.remove(session.getId());
 
         for (Map.Entry<String, WebSocketSession> otherSession : idToActiveSession.entrySet()) { //update userlist upon leaving a user
             JSONObject obj2 = new JSONObject(idAndNick);
-            otherSession.getValue().sendMessage(new TextMessage(obj2.toString()));;
+            otherSession.getValue().sendMessage(new TextMessage(obj2.toString()));
+            otherSession.getValue().sendMessage(new TextMessage(message1.getPayload().replace("Joined","left")));
 
         }
     }
